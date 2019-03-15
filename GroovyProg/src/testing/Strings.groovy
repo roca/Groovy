eeb290973c7fb56c53fa62da87b7e1ed3760ec2a
roca <romel.campbell@regeneron.com>
2019-03-15 16:15:53 -0400

    03152109/16:15

---
+++ GroovyProg/src/testing/Strings.groovy
@@ -0,0 +1,15 @@
+def s = "  Hello Testing World  "
+def s1 = new String("Hello")
+
+
+// Length and Trim
+// def l = s.length()
+println "Length of original String is " + s.length()
+println "Length after trimming spaces is " + s.trim().length()
+
+
+// Upper Case and Lower Case
+println s.toUpperCase()
+println s.toLowerCase()
+
+//
