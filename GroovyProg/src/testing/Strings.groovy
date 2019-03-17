89f800be951109d1a8e7ad5837d96535d274ba4d
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-03-17 05:59:17 -0400

    03172019/05:59

--- GroovyProg/src/testing/Strings.groovy
+++ GroovyProg/src/testing/Strings.groovy
@@ -1,6 +1,6 @@
 def s = "  Hello Testing World  "
 def s1 = new String("Hello")
-
+def s2 = "World"
 
 // Length and Trim
 // def l = s.length()
@@ -12,4 +12,27 @@ println "Length after trimming spaces is " + s.trim().length()
 println s.toUpperCase()
 println s.toLowerCase()
 
-//
+// replace
+println s.replace("Hello","Hi")
+println s.replace('s','$')
+
+// Concatination
+println "  ".concat(s1.concat(" ").concat(s2))
+println "  " + s1 + " " + s2
+
+// To find character at a specific postion
+println "Index postion " + s1.indexOf('o') + " of 'Hello' is a " + s1.charAt(4)
+
+// To find out part of the string
+println "Substring from of index 3 to 7 is " + (s1 + " " + s2).substring(3,8)
+
+def b = "Hello"
+def b1 = "hello"
+
+println  "Ingoring case: ".concat(b.equalsIgnoreCase(b1).toString())
+if (b.equalsIgnoreCase(b1)){
+    println b + " equals " + b1
+} else {
+    println b + " dosn't equal " + b1
+}
+println b.equalsIgnoreCase(b1)
