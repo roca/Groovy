1eb34fd0f989e044085eb180e6c46c491d92cb22
roca <romel.campbell@regeneron.com>
2019-03-18 11:59:39 -0400

    03182019/12noon

--- GroovyProg/src/testing/DataRead.groovy
+++ GroovyProg/src/testing/DataRead.groovy
@@ -6,3 +6,15 @@ while(i != -1) {
     print((char)i)
     i = fr.read()
 }
+
+
+def f2 = new File("./testing/Final.groovy")
+def fr2 = new FileReader(f2)
+def br = new BufferedReader(fr2)
+
+def s = br.readLine()
+
+while(s != null) {
+    println s
+    s = br.readLine()
+}
