e654a51e7eb00b08f0317b4b69af91ff01914359
roca <romel.campbell@regeneron.com>
2019-03-21 09:55:54 -0400

    Getting groovy with Fibonacc part 2

--- GroovyProg/src/testing/C.groovy
+++ GroovyProg/src/testing/C.groovy
@@ -22,3 +22,17 @@ class C {
         }
     }
 }
+
+
+
+class Fibber {
+  def old=1,fib=1,current=1
+
+
+  def next() {
+    def newFib=fib+old
+    old=fib
+    fib=newFib
+    current++
+  }
+}
--- GroovyProg/src/testing/Z.groovy
+++ GroovyProg/src/testing/Z.groovy
@@ -14,9 +14,44 @@ import testing.C
 // def var = this.args[0].toInteger()
 // obj2.checkEvenOdd(var)
 
+
+
+import groovy.time.*
+import groovy.transform.*
+
+
+class MemoizedExample {
+ 
+    @Memoized
+
+    def fib(n) {
+    if (n<2) return 1
+    else return fib(n-1) + fib(n-2)
+    }
+}
+
+
 def var = this.args[0].toInteger()
-def obj = new C()
-obj.checkGrade(var)
+
+// def obj = new C()
+// obj.checkGrade(var)
+
+
+def obj = new MemoizedExample()
+
+
+def timeStart = new Date()
+println obj.fib(var)
+
+def timeStop = new Date()
+TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+
+println duration
+
+
+
+
+
 
 
 
