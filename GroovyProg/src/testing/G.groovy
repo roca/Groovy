603bf167dfb5c88bdbc63eefe4a307b08343d151
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2019-03-25 03:58:46 -0400

    Done with course

--- GroovyProg/src/testing/G.groovy
+++ GroovyProg/src/testing/G.groovy
@@ -6,7 +6,7 @@ class G {
     def isPalendrome(def inputString) {
         def stringReverser = new F()
 
-        if (stringReverser.revString(inputString) == inputString) {
+        if (stringReverser.revString(inputString).equals(inputString)) {
             return true
         }
         return false
